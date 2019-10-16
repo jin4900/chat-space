d446a5025508f0fb213cb712085e7bbc97b6c732
jin <jin.jin.2725kym.ex@gmail.com>
2019-10-16 19:06:33 +0900

    add where not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id: current_user.id)
+    @users = User.where('name LIKE(?)', "%#{user_params[:user]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
