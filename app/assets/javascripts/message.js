216796c7907ef30692f729c271b552955b7d71e7
jin <jin.jin.2725kym.ex@gmail.com>
2019-10-18 21:24:15 +0900

    correction

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,8 +2,7 @@ $(function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="main__messages" data-id="${message.id}">
-                  <div class="main__messages__message">
+    var html = `<div class="main__messages__message"data-id="${message.id}">
                     <div class="main__messages__message__info">
                       <div class="main__messages__message__info__user">
                         ${message.user_name}
@@ -17,8 +16,7 @@ $(function(){
                         ${content}
                       </p>
                       ${img}
-                  </div>
-               </div>`
+                </div>`
   return html;
   }
   $('#new_message').on('submit', function(e){
@@ -36,7 +34,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.main__messages').append(html);
-      $('#message_content').reset(); 
+      $('#new_message')[0].reset(); 
         var target = $('.messages').last();
         var position = $('.messages').scrollTop();
         $('.messages').animate({
@@ -49,31 +47,28 @@ $(function(){
     })
   })
 
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      last_message_id = $('.message:last').data("message-id");
-      $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-        url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-        type: 'get',
-        dataType: 'json',
-      //dataオプションでリクエストに値を含める
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        var insertHTML = '';
-        messages.forEach(function(message){
-          insertHTML = buildHTML(message);
-          $('.messages').append(insertHTML);
-        })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      })
-      .fail(function() {
-        alert('自動更新に失敗しました');
-      });
-    }
-  };
-  setInterval(reloadMessages, 5000);
+  // var reloadMessages = function() {
+  //   if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    
+  //     last_message_id = $('.message:last').data("message-id");
+  //     $.ajax({
+  //       url: "api/messages",
+  //       type: 'GET',
+  //       dataType: 'json',
+  //       data: {id: last_message_id}
+  //     })
+  //     .done(function(messages) {
+  //       var insertHTML = '';
+  //       messages.forEach(function(message){
+  //         insertHTML = buildHTML(message);
+  //         $('.messages').append(insertHTML);
+  //       })
+  //       $('.main__messages').animate({scrollTop: $('.main__messages')[0].scrollHeight}, 'fast');
+  //     })
+  //     .fail(function() {
+  //       alert('自動更新に失敗しました');
+  //     });
+  //   }
+  // };
+  // setInterval(reloadMessages, 5000);
 });
