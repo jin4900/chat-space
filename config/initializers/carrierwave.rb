cbcdbc75c6afd0c0e4557c69fcbbfb27528d0579
jin <jin.jin.2725kym.ex@gmail.com>
2019-10-24 13:29:05 +0900

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,11 +9,12 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1' #例 'ap-northeast-1'
+    region: 'ap-northeast-1' 
   }
 
   config.fog_directory  = 's3pic'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/s3pic'
 end
+
 
 
